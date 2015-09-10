412f3a6d30a62aac79ffb48990f13f2f98560456
Petrica Clement Chiriac <petrica_chiriac@fluxinternet.ro>
2015-09-10 19:38:57 +0300

    Rename ecto tests, add Elixir Map base test

---
+++ test/elixir/map_test.exs
@@ -0,0 +1,55 @@
+defmodule Be2.Elixir.MapTest do
+  use ExUnit.Case, async: false
+
+  defp puts(mess) do
+    IO.puts "\n"
+    IO.puts mess
+  end  
+
+  defp print_map(m) do
+  	 Enum.each m, fn({k,v}) -> 
+  	   IO.puts "#{k} => #{v}" 
+  	 end
+  end
+  
+  defp process_map(m) do
+  	pr_m = 
+  	   m |> Map.delete(:d) |> Map.put(:a, "new")
+  	puts "Print from proces_map(m)"
+  	print_map pr_m
+  	pr_m   
+  end	
+
+
+  test "the_map_work" do
+    m = %{x: 1, y: 2, z: 3, d: "to delete"}
+    
+    puts "Print from test"
+    print_map m
+
+    process_map m
+
+    puts "Print from test again"
+    print_map m
+  end
+
+# Print from test
+# d => to delete
+# x => 1
+# y => 2
+# z => 3
+
+
+# Print from proces_map(m)
+# a => new
+# x => 1
+# y => 2
+# z => 3
+
+
+# Print from test again
+# d => to delete
+# x => 1
+# y => 2
+# z => 3
+end
