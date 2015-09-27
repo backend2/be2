608defe706314b0cd114d3ebdcebc51a45337440
Petrica Clement Chiriac <petrica_chiriac@fluxinternet.ro>
2015-09-27 14:52:39 +0300

    Add open_test_db_persist_data test

--- test/esqlite/open_test.exs
+++ test/esqlite/open_test.exs
@@ -1,5 +1,5 @@
 defmodule Be2.Esqlite.OpenTest do
-  use ExUnit.Case, async: true
+  use ExUnit.Case, async: false
 
   defp rm_file_if_exists(file) do
     if (File.exists? file) do
@@ -22,6 +22,31 @@ defmodule Be2.Esqlite.OpenTest do
     
     assert :ok == :esqlite3.close(db)
     assert true == File.exists?("test.db")
+    rm_file_if_exists "test.db"
+  end
+
+  test "open_test_db_persist_data" do
+    rm_file_if_exists "test.db"
+    
+    {ok, db} = :esqlite3.open('test.db')
+    assert true == File.exists?("test.db")
+
+    assert :ok == :esqlite3.exec('CREATE TABLE tbl (id integer NOT NULL)', db)
+    assert :ok == :esqlite3.exec('INSERT INTO tbl VALUES (1)', db)
+    assert [{1}] == :esqlite3.q('SELECT id FROM tbl', db)
+    assert [{1}] == :esqlite3.q('SELECT count(*) FROM tbl', db)
+    assert :ok == :esqlite3.exec('INSERT INTO tbl VALUES (2)', db)
+    assert [{1}, {2}] == :esqlite3.q('SELECT id FROM tbl ORDER BY id', db)
+    assert [{2}] == :esqlite3.q('SELECT count(*) FROM tbl', db)
+
+    assert :ok == :esqlite3.close(db)
+    # Open again
+    {ok, db} = :esqlite3.open('test.db')
+    assert true == File.exists?("test.db")
+    assert [{1}, {2}] == :esqlite3.q('SELECT id FROM tbl ORDER BY id', db)
+    assert [{2}] == :esqlite3.q('SELECT count(*) FROM tbl', db)
+    assert :ok == :esqlite3.close(db)
+
     rm_file_if_exists "test.db"
   end
 
